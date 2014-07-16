778a9250850177ab8f8f547c8bb8b80bdd98fac9
stevepm <steven.magelowitz@gmail.com>
2014-07-15 21:53:34 -0600

    Updated from module to a class

--- lib/ruby_gems.rb
+++ lib/ruby_gems.rb
@@ -2,16 +2,8 @@ require "RubyGemsApi/version"
 require 'faraday'
 require 'json'
 
-module RubyGemsApi
-  class << self
-    def instance
-      ins = Object.new
-      ins.extend RubyGemsApi
-      ins
-    end
-  end
-
-  def look_up(gem_name)
+class RubyGems
+  def initialize(gem_name)
     @response = Faraday.get "https://rubygems.org/api/v1/gems/#{gem_name}.json"
     @body = JSON.parse(@response.body)
   end
--- spec/rubygemsapi_spec.rb
+++ spec/rubygemsapi_spec.rb
@@ -1,34 +1,32 @@
 require 'spec_helper'
 
-describe RubyGemsApi do
-  let (:faraday) { RubyGemsApi.instance }
+describe RubyGems do
   before do
     VCR.use_cassette('faraday') do
-
-      faraday.look_up('faraday')
+      @faraday= RubyGems.new("faraday")
     end
   end
   it 'can connect to the Ruby Gems API' do
-    expect(faraday.status).to eq(200)
+    expect(@faraday.status).to eq(200)
   end
 
   it 'can return the total downloads' do
-    expect(faraday.downloads).to eq(9585597)
+    expect(@faraday.downloads).to eq(9588400)
   end
 
   it 'can return the current version' do
-    expect(faraday.version).to eq("0.9.0")
+    expect(@faraday.version).to eq("0.9.0")
   end
 
   it 'can return version downloads' do
-    expect(faraday.version_downloads).to eq(1201239)
+    expect(@faraday.version_downloads).to eq(1202947)
   end
 
-  it 'can return the urls' do
-    expect(faraday.urls).to eq({
-                                 :gem_uri=>"http://rubygems.org/gems/faraday-0.9.0.gem",
-                                 :homepage_uri=>"https://github.com/lostisland/faraday",
-                                 :project_uri=>"http://rubygems.org/gems/faraday"
+  it 'can return the urls in a hash' do
+    expect(@faraday.urls).to eq({
+                                 :gem_uri => "http://rubygems.org/gems/faraday-0.9.0.gem",
+                                 :homepage_uri => "https://github.com/lostisland/faraday",
+                                 :project_uri => "http://rubygems.org/gems/faraday"
                                })
   end
 end
